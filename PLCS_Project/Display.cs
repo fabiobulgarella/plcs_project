e9f647a5357655a27fc2729847f1c02c98ead420
Fabio Bulgarella <fabio.bulgarella@gmail.com>
2018-05-25 12:03:48 +0200

    Other changes

---
+++ PLCS_Project/Display.cs
@@ -0,0 +1,40 @@
+using System;
+using Microsoft.SPOT;
+using Gadgeteer.Modules.GHIElectronics;
+using GT = Gadgeteer;
+
+namespace PLCS_Project
+{
+    class Display
+    {
+        private DisplayTE35 display;
+
+        public Display(DisplayTE35 display)
+        {
+            this.display = display;
+        }
+
+        public void UpdateSensorData(double tempC, double pressureMb, double relativeHumidity)
+        {
+            string temp = "Temperature: " + tempC.ToString("F2") + " C°";
+            string pressure = "Pressure: " + pressureMb.ToString("F2") + " mBar";
+            string humidity = "Relative Humidity: " + relativeHumidity.ToString("F2") + " %";
+            display.SimpleGraphics.DisplayRectangle(GT.Color.Black, 0, GT.Color.Black, 0, 72, 240, 54);
+            display.SimpleGraphics.DisplayText(temp, Resources.GetFont(Resources.FontResources.NinaB), GT.Color.LightGray, 0, 72);
+            display.SimpleGraphics.DisplayText(pressure, Resources.GetFont(Resources.FontResources.NinaB), GT.Color.LightGray, 0, 90);
+            display.SimpleGraphics.DisplayText(humidity, Resources.GetFont(Resources.FontResources.NinaB), GT.Color.LightGray, 0, 107);
+            display.SimpleGraphics.Redraw();
+        }
+
+        public void UpdateMouseData(int exceptionCounter, string position, string millimetersPosition)
+        {
+            string toPrint = "Exceptions raised: " + exceptionCounter;
+            display.SimpleGraphics.DisplayRectangle(GT.Color.Black, 0, GT.Color.Black, 0, 0, 240, 71);
+            display.SimpleGraphics.DisplayText("Cursor Position", Resources.GetFont(Resources.FontResources.NinaB), GT.Color.LightGray, 0, 0);
+            display.SimpleGraphics.DisplayText(position, Resources.GetFont(Resources.FontResources.NinaB), GT.Color.LightGray, 0, 18);
+            display.SimpleGraphics.DisplayText(millimetersPosition, Resources.GetFont(Resources.FontResources.NinaB), GT.Color.LightGray, 0, 36);
+            display.SimpleGraphics.DisplayText(toPrint, Resources.GetFont(Resources.FontResources.NinaB), GT.Color.LightGray, 0, 54);
+            display.SimpleGraphics.Redraw();
+        }
+    }
+}
